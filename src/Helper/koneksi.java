819dd19c006423590b8f06ea4898868b18af2f9d
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2023-05-14 22:02:55 +0700

    up

--- src/Helper/koneksi.java
+++ src/Helper/koneksi.java
@@ -4,10 +4,35 @@
  */
 package Helper;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import com.mysql.cj.jdbc.MysqlDataSource;
+
 /**
  *
  * @author LENOVO
  */
 public class koneksi {
+    static Connection con;
     
+    public static Connection getConnection()
+    {
+        if(con == null)
+        {
+            MysqlDataSource data = new MysqlDataSource();
+            data.setDatabaseName("db_rental");
+            data.setUser("root");
+            data.setPassword("");
+            try
+            {
+                con = data.getConnection();
+                System.out.println("berhasil connect");
+            }catch(SQLException e)
+            {
+                System.out.println("tidk connect");
+            }
+                
+        }
+        return con;
+    }
 }
--- src/rentalkendaraan/RentalKendaraan.java
+++ src/rentalkendaraan/RentalKendaraan.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package rentalkendaraan;
+import Helper.koneksi;
 
 /**
  *
@@ -15,6 +16,7 @@ public class RentalKendaraan {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        koneksi.getConnection();
     }
     
 }
