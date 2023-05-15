d553740e55d68bb81f2d5a1411e491879cb05d78
allatifatul <allatifatulmuharomiah@students.unnes.ac.id>
2023-05-15 13:47:42 +0700

    update model

--- src/Model/Kendaraan.java
+++ src/Model/Kendaraan.java
@@ -9,7 +9,125 @@ package Model;
  * @author LENOVO
  */
 public class Kendaraan {
-    
+     /**
+     * @return the id_k
+     */
+    public Integer getId_k() {
+        return id_k;
+    }
+
+    /**
+     * @param id_k the id_k to set
+     */
+    public void setId_k(Integer id_k) {
+        this.id_k = id_k;
+    }
+
+    /**
+     * @return the jenis
+     */
+    public String getJenis() {
+        return jenis;
+    }
+
+    /**
+     * @param jenis the jenis to set
+     */
+    public void setJenis(String jenis) {
+        this.jenis = jenis;
+    }
+
+    /**
+     * @return the merek
+     */
+    public String getMerek() {
+        return merek;
+    }
+
+    /**
+     * @param merek the merek to set
+     */
+    public void setMerek(String merek) {
+        this.merek = merek;
+    }
+
+    /**
+     * @return the tipe
+     */
+    public String getTipe() {
+        return tipe;
+    }
+
+    /**
+     * @param tipe the tipe to set
+     */
+    public void setTipe(String tipe) {
+        this.tipe = tipe;
+    }
+
+    /**
+     * @return the tahun
+     */
+    public String getTahun() {
+        return tahun;
+    }
+
+    /**
+     * @param tahun the tahun to set
+     */
+    public void setTahun(String tahun) {
+        this.tahun = tahun;
+    }
+    /**
+     * @return the nomor
+     */
+    public String getNomor() {
+        return nomor;
+    }
+
+    /**
+     * @param nomor the nomor to set
+     */
+    public void setNomor(String nomor) {
+        this.nomor = nomor;
+    }
+
+    /**
+     * @return the harga
+     */
+    public Integer getHarga() {
+        return harga;
+    }
+
+    /**
+     * @param harga the harga to set
+     */
+    public void setHarga(Integer harga) {
+        this.harga = harga;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    private Integer id_k;
+    private String jenis;
+    private String merek;
+    private String tipe;
+    private String tahun;
+    private String nomor;
+    private Integer harga;
+    private String status;
 
     
 }
